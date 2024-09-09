68ca5a56863eafdc510ea9b5fed022c069588dd7
timote-koenig-Adeo <timote.koenig@adeo.com>
2024-09-09 15:00:08 +0200

    feat: display history with appendHistory javascript function

--- script.js
+++ script.js
@@ -27,3 +27,45 @@ document.addEventListener('click', function(event) {
         offSearch()
     }
 });
+
+const appendHistory = (input, historyList) => {
+    let newListItem = document.createElement('li');
+    newListItem.classList.add('history_element');
+
+    let historyLink = document.createElement('a');
+    historyLink.classList.add('history_link');
+    historyLink.href = "https://www.leroymerlin.fr/";
+    historyLink.target = "_blank";
+
+    let historyIcon = document.createElement('img');
+    historyIcon.src = "img/clock-rotate-left-solid.svg";
+    historyIcon.alt = "historique";
+    historyIcon.height = 20;
+
+    let historyInput = document.createElement('div');
+    historyInput.textContent  = input;
+    historyInput.classList.add('history_input');
+
+    historyLink.appendChild(historyIcon);
+    historyLink.appendChild(historyInput);
+
+    newListItem.appendChild(historyLink);
+
+    historyList.insertBefore(newListItem, historyList.firstChild);
+};
+
+const inputIsOkay = (input) => {
+    return input !== ""
+}
+
+document.getElementById('searchbar').addEventListener('keypress', function(event) {
+    const historyList = document.getElementById('history_list');
+    let input = document.getElementById('searchbar').value;
+
+    if (event.key === 'Enter' && inputIsOkay(input)) {
+        if (historyList.childElementCount === 6)
+            historyList.removeChild(historyList.lastChild);
+        if (historyList.childElementCount < 6)
+            appendHistory(input, historyList);
+    }
+});
