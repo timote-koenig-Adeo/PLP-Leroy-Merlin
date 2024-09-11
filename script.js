0c9dc995be64e95876834b0923465465bf6fb38d
timote-koenig-Adeo <timote.koenig@adeo.com>
2024-09-11 09:14:39 +0200

    feat: burger menu on navigation header and gray background + refactor all value, replace '_' by '-'

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 const searchbar = document.getElementById('searchbar');
 
 const onSearch = () => {
-    document.getElementById('searchbar_container').style.backgroundColor = 'rgba(255,255,255,100%)';
-    document.getElementById('overlay').style.display = "flex";
-    document.getElementById('history_list').style.display = "flex";
-    document.getElementById('history_list').style.zIndex = "4";
-    document.querySelector('.history_link').style.zIndex = '4';
+    document.getElementById('searchbar-container').style.backgroundColor = 'rgba(255,255,255,100%)';
+    document.getElementById('history-list').style.display = "flex";
+    document.getElementById('history-list').style.zIndex = "4";
+    document.querySelector('.history-link').style.zIndex = '4';
 };
 
 const offSearch = () => {
-    document.getElementById('searchbar_container').style.backgroundColor = 'rgba(255,255,255,0%)';
-    document.getElementById('overlay').style.display = "none";
-    document.getElementById('history_list').style.display = "none";
-    document.getElementById('history_list').style.zIndex = "-1";
-    document.querySelector('.history_link').style.zIndex = '-1';
+    document.getElementById('searchbar-container').style.backgroundColor = 'rgba(255,255,255,0%)';
+    document.getElementById('history-list').style.display = "none";
+    document.getElementById('history-list').style.zIndex = "-1";
+    document.querySelector('.history-link').style.zIndex = '-1';
 };
 
 document.addEventListener('click', function(event) {
@@ -30,10 +28,10 @@ document.addEventListener('click', function(event) {
 
 const appendHistory = (input, historyList) => {
     let newListItem = document.createElement('li');
-    newListItem.classList.add('history_element');
+    newListItem.classList.add('history-element');
 
     let historyLink = document.createElement('a');
-    historyLink.classList.add('history_link');
+    historyLink.classList.add('history-link');
     historyLink.href = "https://www.leroymerlin.fr/";
     historyLink.target = "_blank";
 
@@ -44,7 +42,7 @@ const appendHistory = (input, historyList) => {
 
     let historyInput = document.createElement('div');
     historyInput.textContent  = input;
-    historyInput.classList.add('history_input');
+    historyInput.classList.add('history-input');
 
     historyLink.appendChild(historyIcon);
     historyLink.appendChild(historyInput);
@@ -59,7 +57,7 @@ const inputIsOkay = (input) => {
 }
 
 document.getElementById('searchbar').addEventListener('keypress', function(event) {
-    const historyList = document.getElementById('history_list');
+    const historyList = document.getElementById('history-list');
     let input = document.getElementById('searchbar').value;
 
     if (event.key === 'Enter' && inputIsOkay(input)) {
@@ -69,3 +67,46 @@ document.getElementById('searchbar').addEventListener('keypress', function(event
             appendHistory(input, historyList);
     }
 });
+
+
+// show or hide burger-menu
+const navigationItems = document.querySelectorAll('.navigation-list li');
+
+navigationItems.forEach(item => {
+
+    const menuId = item.dataset.menuId;
+    const menu = document.getElementById(menuId);
+
+    let isMenuHovered = false;
+    let isItemHovered = false;
+
+    item.addEventListener('mouseover', () => {
+        menu.classList.add('burger-menu-open');
+        isItemHovered = true;
+    });
+
+    item.addEventListener('mouseout', () => {
+        isItemHovered = false;
+        setTimeout(() => {
+            closeMenuIfNeeded();
+        }, 0);
+    });
+
+    menu.addEventListener('mouseover', () => {
+        isMenuHovered = true;
+    });
+
+    menu.addEventListener('mouseout', () => {
+        isMenuHovered = false;
+        setTimeout(() => {
+            closeMenuIfNeeded();
+        }, 0);
+    });
+
+    function closeMenuIfNeeded() {
+        if (!isMenuHovered && !isItemHovered) {
+            menu.classList.remove('burger-menu-open');
+        }
+    }
+});
+
